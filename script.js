ce71bcd9d2f89afc3b4ebed32d7a4b888b3f9138
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-07 06:30:38 -0300

    Set some CSS properties on the grid container

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 // STORE THE GRID CONTAINER
 
 const gridContainer = document.querySelector("div.container");
+
+// SET THE GRID'S WIDTH AND HEIGHT
+
+gridContainer.style.width = "720px";
+gridContainer.style.height = "720px";
+gridContainer.style.outline = "3px solid black";
+gridContainer.style.display = "flex";
+gridContainer.style.flexWrap = "wrap";
