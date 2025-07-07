9aa6552ff3f0b76ea597c94a3b0c6472a504a80f
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2025-07-07 06:34:48 -0300

    Create the cells for the grid

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ gridContainer.style.height = "720px";
 gridContainer.style.outline = "3px solid black";
 gridContainer.style.display = "flex";
 gridContainer.style.flexWrap = "wrap";
+
+// CREATE A CELL FOR THE GRID
+
+function createCell() {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  cell.style.width = "45px";
+  cell.style.height = "45px";
+  cell.style.border = "2px solid gray";
+  return cell;
+}
+
+for (let i = 0; i < 256; i++) {
+  const cell = createCell();
+  gridContainer.appendChild(cell);
+  cell.addEventListener("mouseenter", (e) => {
+    e.target.style.backgroundColor = "deeppink";
+  });
+}
